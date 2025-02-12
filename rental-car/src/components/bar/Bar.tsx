00696d1b82b581e5391ac1e1d0ad46f3504a6a74
@dovish770 <dovish770@gmail.com>
2025-02-12 16:35:49 +0200

    fixin

--- rental-car/src/components/bar/Bar.tsx
+++ rental-car/src/components/bar/Bar.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -11,10 +10,16 @@ import Link from 'next/link';
 export default function Bar() {
   return (
     <div  className={styles.barContainer}>
-      <AppBar >
+      <AppBar>
         <Toolbar className={styles.toolBar}>
-          <Button color="inherit"><Link href="/pages/logIn" replace>התחבר</Link></Button>
-          <IconButton className={styles.iconButton}>
+          <Button color="inherit"><Link href="/pages/logIn" replace className={styles.link}>התחבר</Link></Button>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
             <MenuIcon />
           </IconButton>
         </Toolbar>
